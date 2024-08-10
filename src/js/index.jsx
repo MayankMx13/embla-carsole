0d059be1da5ab5a1d58a44a9fa9f6bcad6bd2295
Mayank mishra <153012903+MayankMx13@users.noreply.github.com>
2024-08-10 14:39:59 +0530

    Initial commit

---
+++ src/js/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EmblaCarousel from './EmblaCarousel'
+import Header from './Header'
+
+import '../css/base.css'
+import '../css/sandbox.css'
+import '../css/embla.css'
+
+const OPTIONS = { loop: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+const App = () => (
+  <>
+    <Header />
+    <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+  </>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
